9bce1b4245dc76a9c7dfca50dd8d24bb1cae3ac6
offpudding <offpudding@users.noreply.github.com>
2018-07-10 12:19:01 -0400

    Add files via upload
    Reorganized initial classes. Changed Vector class to facilitate dimensionally generic Voronoi subdivisions

---
+++ CGULib/Classes/LineSegment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CGULib.Classes
+{
+    public class LineSegment<T> where T : VectorBase
+    {
+        HalfEdge<T> p1;
+        HalfEdge<T> p2;
+    }
+}
