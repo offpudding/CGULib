c198e692ab8507abf17a404e531b07e2d81506b7
offpudding <offpudding@users.noreply.github.com>
2018-07-10 12:26:01 -0400

    Add files via upload
    fixing? commit mistake

---
+++ CGULib/CGULib/Classes/VectorBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CGULib.Classes
+{
+
+    public abstract class VectorBase
+    {
+        protected double[] values;
+
+        public double[] Values { get => values; }
+        public int Length { get => values.Length; }
+
+        //TODO: Vector properties
+
+        public VectorBase(double[] values)
+        {
+            this.values = values;
+        }
+    }
+    
+
+}
---
+++ CGULib/CGULib/Statics/Voronoi/Voronoi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CGULib.Classes;
+
+namespace CGULib.Voronoi
+{
+    public static class Voronoi
+    {
+        public static DCEL<T> VoronoiSubdivideRegion<T>(DCEL<T> region, List<T> points, bool isEuclidian = true) where T : VectorBase
+        {
+            return new DCEL<T>("voronoi", "this is a voronoi subdivision");
+        }
+    }
+}
