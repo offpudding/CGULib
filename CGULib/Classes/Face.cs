f010349de5bc2a17773551be3a479fad1004eb3f
offpudding <offpudding@users.noreply.github.com>
2018-07-10 12:19:01 -0400

    Add files via upload
    Reorganized initial classes. Changed Vector class to facilitate dimensionally generic Voronoi subdivisions

---
+++ CGULib/Classes/Face.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CGULib.Classes
+{
+    public class Face<T> where T : VectorBase
+    {
+        HalfEdge<T> outterEdge;
+        HalfEdge<T>[] holes;
+
+        //TODO: Region properties
+    }
+}
